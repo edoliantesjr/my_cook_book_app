bc38546cf8fea4401e24d70d71cd8fc980a250c4
edoliantesjr <edoliantesjr@gmail.com>
2022-06-15 20:59:52 +0800

    added refresh on home screen

--- lib/ui/views/home_view/home_view.dart
+++ lib/ui/views/home_view/home_view.dart
@@ -38,87 +38,101 @@ class HomeView extends StatelessWidget {
           ],
         ),
         backgroundColor: Colors.green.shade50,
-        body: ListView(
-          padding: EdgeInsets.all(18),
-          primary: true,
-          children: [
-            Text('What are you looking for today?'),
-            SizedBox(height: 8),
-            GestureDetector(
-              onTap: () => model.goToSearchView(),
-              child: SizedBox(
-                height: 45,
-                child: TextField(
-                  enabled: false,
-                  decoration: InputDecoration(
-                    hintText: 'Search recipes..',
-                    constraints: BoxConstraints(minHeight: 45, maxHeight: 45),
-                    prefixIcon: Icon(Icons.search),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+        body: RefreshIndicator(
+          onRefresh: () async {
+            model.init();
+          },
+          child: ListView(
+            padding: EdgeInsets.all(18),
+            primary: true,
+            children: [
+              Text('What are you looking for today?'),
+              SizedBox(height: 8),
+              GestureDetector(
+                onTap: () => model.goToSearchView(),
+                child: SizedBox(
+                  height: 45,
+                  child: TextField(
+                    enabled: false,
+                    decoration: InputDecoration(
+                      hintText: 'Search recipes..',
+                      constraints: BoxConstraints(minHeight: 45, maxHeight: 45),
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      contentPadding: EdgeInsets.zero,
                     ),
-                    contentPadding: EdgeInsets.zero,
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-            model.isBusy
-                ? Center(
-                    child: Container(
-                      height: 500,
-                      child: Center(
-                        child: CircularProgressIndicator(),
+              SizedBox(height: 10),
+              model.isBusy
+                  ? Center(
+                      child: Container(
+                        height: 500,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            CircularProgressIndicator(),
+                            SizedBox(height: 10),
+                            Text(
+                              'Fetching Wonderful Recipes.\nPlease wait...',
+                              textAlign: TextAlign.center,
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                  )
-                : ListView.separated(
-                    //should not use listview with shrinkwrap
-                    //not efficient, this is temporary only
-                    key: ObjectKey(model.feedRecipeSection),
-                    shrinkWrap: true,
-                    primary: false,
-                    itemBuilder: (context, featureIndex) => Column(
-                      key: ObjectKey(model.feedRecipeSection[featureIndex]),
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          model.feedRecipeSection[featureIndex].name ??
-                              'Featured',
-                          style: GoogleFonts.poppins(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        Container(
-                          height: 265,
-                          child: ListView.separated(
-                            //should not use listview with shrinkwrap
-                            //not efficient, this is temporary only
-                            shrinkWrap: true,
-                            primary: false,
-                            physics: BouncingScrollPhysics(),
-                            scrollDirection: Axis.horizontal,
-                            itemCount: model.feedRecipeSection[featureIndex]
-                                .recipes!.length,
-                            itemBuilder: (context, index) => RecipeCard(
-                              key: ObjectKey(model
-                                  .feedRecipeSection[featureIndex]
-                                  .recipes?[index]),
-                              recipe: model.feedRecipeSection[featureIndex]
-                                  .recipes![index],
+                    )
+                  : ListView.separated(
+                      //should not use listview with shrinkwrap
+                      //not efficient, this is temporary only
+                      key: ObjectKey(model.feedRecipeSection),
+                      shrinkWrap: true,
+                      primary: false,
+                      itemBuilder: (context, featureIndex) => Column(
+                        key: ObjectKey(model.feedRecipeSection[featureIndex]),
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            model.feedRecipeSection[featureIndex].name ??
+                                'Featured',
+                            style: GoogleFonts.poppins(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
                             ),
-                            separatorBuilder: (context, index) =>
-                                SizedBox(height: 4),
                           ),
-                        ),
-                      ],
-                    ),
-                    separatorBuilder: (context, index) => SizedBox(height: 2),
-                    itemCount: model.feedRecipeSection.length,
-                  )
-          ],
+                          Container(
+                            height: 265,
+                            child: ListView.separated(
+                              //should not use listview with shrinkwrap
+                              //not efficient, this is temporary only
+                              shrinkWrap: true,
+                              primary: false,
+                              physics: BouncingScrollPhysics(),
+                              scrollDirection: Axis.horizontal,
+                              itemCount: model.feedRecipeSection[featureIndex]
+                                  .recipes!.length,
+                              itemBuilder: (context, index) => RecipeCard(
+                                key: ObjectKey(model
+                                    .feedRecipeSection[featureIndex]
+                                    .recipes?[index]),
+                                recipe: model.feedRecipeSection[featureIndex]
+                                    .recipes![index],
+                              ),
+                              separatorBuilder: (context, index) =>
+                                  SizedBox(height: 4),
+                            ),
+                          ),
+                        ],
+                      ),
+                      separatorBuilder: (context, index) => SizedBox(height: 2),
+                      itemCount: model.feedRecipeSection.length,
+                    )
+            ],
+          ),
         ),
       ),
     );
